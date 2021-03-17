1e0fe2851c1ccb53e38b9be3324420de0c71188d
Nicole <nicole.carvajal.b@gmail.com>
2021-03-16 18:20:26 -0600

    Inicio función scheduling y sig_alarm_handler

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <setjmp.h>
 
 #define UNIDAD_TRABAJO 50
 
@@ -26,6 +28,8 @@ struct Thread{
 //Puntero a arreglo de Threads de tamaño TOTAL_THREADS
 struct Thread *threads;
 
+sigjmp_buf jmpbuf;
+
 void new_thread(struct Thread *thread, int boletos){
     thread->total_boletos = boletos;
     thread->resultado_parcial_de_pi = 0;
@@ -33,7 +37,7 @@ void new_thread(struct Thread *thread, int boletos){
     thread->unidades_de_trabajo_pendientes = 0;
 }
 
-
+//
 void read_parameters()
 {
     FILE *file;
@@ -90,11 +94,43 @@ void read_parameters()
     
 }
 
+int todos_los_threads_terminaron()
+{
+    return 0;
+}
+
+void sig_alarm_handler(int sigo)
+{
+    siglongjmp(jmpbuf, 2);
+}
+
+void lottery_scheduler()
+{
+    sigsetjmp(jmpbuf, 1);
+    if(todos_los_threads_terminaron()) exit(0);
+    
+    else
+    {
+        int total_boletos = 0;
+        
+    }
+	
+}
+
+
+
 int main(int argc, char **argv)
 {
     
     read_parameters();
     
+    //Asigna función a alarma
+    if(signal(SIGALRM, sig_alarm_handler) == SIG_ERR){
+    	printf("Error de la señal");
+    	exit(1);
+    }
     
+    lottery_scheduler();
+        
     return 0;
 }
